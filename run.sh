b3bdb7c35138cb3d5540324313bb6ba42e517f1c
Jermine <jermine.hu@qq.com>
2017-04-26 11:18:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 # sh /startup.sh && sh catalina.sh run
 
-/usr/local/mysql/bin/mysqld_safe --user=root & /catalina.sh run
+# /usr/local/mysql/bin/mysqld_safe --user=root & /catalina.sh run
+
+/usr/bin/mysqld_safe --user=root  & /catalina.sh run
