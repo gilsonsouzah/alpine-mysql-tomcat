8f1612aa02e10a8af9ceda4d959869d717924e50
Jermine Hu <jermine.hu@qq.com>
2017-04-26 16:05:18 +0800

    tadd shell for mysql init before start

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # /usr/bin/mysqld_safe --user=root & $CATALINA_HOME/bin/catalina.sh run
 # /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
- ／entrypoint.sh & /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
+ /entrypoint.sh & /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
