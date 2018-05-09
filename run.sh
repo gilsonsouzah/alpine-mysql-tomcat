7a91e2069aa7f2272748552b2e60870bac54aa72
Gilson F. B. Souza <gilsonfernandesbatista@gmail.com>
2018-05-09 16:26:03 -0300

    Correção de permissões

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ RET=1
 while [[ RET -ne 0 ]]; do
     echo "=> Waiting for confirmation of MySQL service startup"
     sleep 5
-    mysql -uroot -e "status" > /dev/null 2>&1
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "status" > /dev/null 2>&1
     RET=$?
 done
 
@@ -14,4 +14,4 @@ if [ -f /dump.sql ]; then
     mysql -uroot -p$MYSQL_ROOT_PASSWORD < /dump.sql
 fi
 
-exec $CATALINA_HOME/bin/catalina.sh run
+$CATALINA_HOME/bin/catalina.sh run
