c1ff1afdfcd198af8941e22e39348232108c9d34
Gilson F. B. Souza <gilsonfernandesbatista@gmail.com>
2018-05-09 14:39:03 -0300

    Correção de permissões

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-# /usr/bin/mysqld_safe --user=root & $CATALINA_HOME/bin/catalina.sh run
-# /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
-# /entrypoint.sh & /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
-/usr/bin/mysqld --user=root --console & /build.sh
+
+if [ -f /dump.sql ]; then
+    echo "[i] Init file found: Loading database with initial script"
+    /usr/bin/mysqld --user=root --init-file=/dump.sql --console & $CATALINA_HOME/bin/catalina.sh run
+else
+    echo "[i] Init file NOT found: Loading database ONLY"
+    /usr/bin/mysqld --user=root --console & $CATALINA_HOME/bin/catalina.sh run
+fi
+
