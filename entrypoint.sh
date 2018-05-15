63220b41548ad80a4ae7e9afc8fd72b9f8eaee95
Gilson F. B. Souza <gilsonfernandesbatista@gmail.com>
2018-05-15 17:08:27 -0300

    fix: Corrigindo fluxo de inicialização do mysql no container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,57 +1,60 @@
 #!/bin/sh
 
 if [ -d /app/mysql ]; then
-  echo "[i] MySQL directory already present, skipping creation"
+    echo "[i] MySQL directory already present, skipping creation"
 else
-  echo "[i] MySQL data directory not found, creating initial DBs"
+    echo "[i] MySQL data directory not found, creating initial DBs"
 
-  mysql_install_db --user=root > /dev/null
+    mysql_install_db --user=root > /dev/null
 
-  if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-    MYSQL_ROOT_PASSWORD=47PY7rpS5wfprCtY5nvvqxYD2GU
-    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-  fi
-
-  MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-  MYSQL_USER=${MYSQL_USER:-""}
-  MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
-
-  if [ ! -d "/run/mysqld" ]; then
-    mkdir -p /run/mysqld
-  fi
-
-  tfile=`mktemp`
-  if [ ! -f "$tfile" ]; then
-      return 1
-  fi
-
-  cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
-CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.%.%.%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-#UPDATE user SET authentication_string=PASSWORD("") WHERE user='root' AND host='localhost';
-UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
-FLUSH PRIVILEGES;
-EOF
-
-  if [ "$MYSQL_DATABASE" != "" ]; then
-    echo "[i] Creating database: $MYSQL_DATABASE"
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-
-    if [ "$MYSQL_USER" != "" ]; then
-      echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-      echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+    if [ "$MYSQL_PASSWORD" = "" ]; then
+    MYSQL_PASSWORD=47PY7rpS5wfprCtY5nvvqxYD2GU
+    echo "[i] MySQL admin Password: $MYSQL_PASSWORD"
     fi
-  fi
 
-  /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
+    MYSQL_DATABASE=${MYSQL_DATABASE:-""}
 
+    if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+    fi
 
-  rm -f $tfile
+    /usr/bin/mysqld_safe --user=root --bootstrap > /dev/null 2>&1 &
+
+    RET=1
+    while [[ RET -ne 0 ]]; do
+      echo "[i] Waiting for confirmation of MySQL service startup"
+      sleep 5
+      mysql -uroot -e "status" > /dev/null 2>&1
+      RET=$?
+    done
+
+    echo "[i]  Creating MySQL admin user with ${MYSQL_PASSWORD} password"
+
+    mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
+
+    if [ "$MYSQL_DATABASE" = "" ]; then
+    echo "[i]  Creating MySQL database with name ${MYSQL_DATABASE}"
+    mysql -uroot -e "CREATE SCHEMA '$MYSQL_DATABASE'"
+    fi
+
+    if [ -f /dump.sql ] ; then
+      echo "[i] Initial script found, running"
+      mysql -uadmin -p$MYSQL_PASSWORD
+    fi
+
+    echo "=> Done!"
+
+    echo "========================================================================"
+    echo "You can now connect to this MySQL Server using:"
+    echo ""
+    echo "    mysql -uadmin -p$MYSQL_PASSWORD -h<host> -P<port>"
+    echo ""
+    echo "Please remember to change the above password as soon as possible!"
+    echo "MySQL user 'root' has no password but only allows local connections"
+    echo "========================================================================"
+
+    mysqladmin -uroot shutdown
 fi
 
 
--- run.sh
+++ run.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
-echo "=> Starting MySQL Daemon process"
-rc-service mariadb start
 
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "status" > /dev/null 2>&1
-    RET=$?
-done
-
-if [ -f /dump.sql ]; then
-    echo "=> Dump file found, seeding database"
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD < /dump.sql
-fi
-
-$CATALINA_HOME/bin/catalina.sh run
+# /usr/bin/mysqld_safe --user=root & $CATALINA_HOME/bin/catalina.sh run
+# /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
+# /entrypoint.sh & /usr/bin/mysqld --user=root  & $CATALINA_HOME/bin/catalina.sh run
+/usr/bin/mysqld --user=root --console & $CATALINA_HOME/bin/catalina.sh run
